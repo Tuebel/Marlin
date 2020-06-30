a3e00e33d9e9ebe9c6b2ce8a4818fac338998dce
Tim Übelhör <TimUebelhoer@live.de>
2020-06-30 11:26:49 +0200

    Fine tune for CURA

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -974,7 +974,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -55, -12, -3.13 }
+#define NOZZLE_TO_PROBE_OFFSET { -55, -12, -3.10 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
