2021d3c88549dbba938bc3bf2c0f76b9e502c5d7
Tim <timuebelhoer@live.de>
2021-01-10 16:56:38 +0100

    Can use firmware z-hop again

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1998,12 +1998,12 @@
   #endif
   #define RETRACT_LENGTH 0.8              // (mm) Default retract length (positive value)
   #define RETRACT_LENGTH_SWAP 13          // (mm) Default swap retract length (positive value)
-  #define RETRACT_FEEDRATE 25             // (mm/s) Default feedrate for retracting
-  #define RETRACT_ZRAISE 0.0             // (mm) Default retract Z-raise
+  #define RETRACT_FEEDRATE 30             // (mm/s) Default feedrate for retracting
+  #define RETRACT_ZRAISE 0.1              // (mm) Default retract Z-raise
   #define RETRACT_RECOVER_LENGTH 0        // (mm) Default additional recover length (added to retract length on recover)
   #define RETRACT_RECOVER_LENGTH_SWAP 0   // (mm) Default additional swap recover length (added to retract length on recover from toolchange)
-  #define RETRACT_RECOVER_FEEDRATE 25      // (mm/s) Default feedrate for recovering from retraction
-  #define RETRACT_RECOVER_FEEDRATE_SWAP 25 // (mm/s) Default feedrate for recovering from swap retraction
+  #define RETRACT_RECOVER_FEEDRATE 30      // (mm/s) Default feedrate for recovering from retraction
+  #define RETRACT_RECOVER_FEEDRATE_SWAP 30 // (mm/s) Default feedrate for recovering from swap retraction
   #if ENABLED(MIXING_EXTRUDER)
     //#define RETRACT_SYNC_MIXING         // Retract and restore all mixing steppers simultaneously
   #endif
